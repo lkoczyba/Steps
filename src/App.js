950faef4c05111cd59d41faa770a76d29f2efaa8
lkoczyba <lukasz.koczyba@gmail.com>
2023-08-06 21:30:43 +0200

    build steps component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
- const App = ()=>{
-  return(<div>Hello React</div>);
-}
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+const App = () => {
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
 
- export default App
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
